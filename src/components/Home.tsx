367d19c7c090ba1f3cce5cd78db7c8b30ecd76f8
Eldor <eldor@Eldors-Air.attlocal.net>
2023-08-07 02:00:15 -0700

    Adds new logo in 3D with drei and lighting and scroll feature

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,52 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
+import ThreeScene from '../components/ThreeScene';
+import Navbar from './Navbar';
 
-const Home: React.FC = () => {
-  const [isChatOpen, setIsChatOpen] = useState(false);
-
-  const toggleChat = () => {
-    setIsChatOpen(!isChatOpen);
-  };
-
+const Home = () => {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      {/* Navbar */}
-      <nav className="bg-white shadow-md">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between h-16 items-center">
-            <div className="flex items-center">
-              <span className="font-semibold text-xl text-gray-800">About Me</span>
-            </div>
-            <div className="flex items-center">
-              <button
-                className="text-gray-600 hover:text-gray-800 focus:outline-none"
-                onClick={toggleChat}
-              >
-                Open Chat
-              </button>
-            </div>
-          </div>
-        </div>
-      </nav>
-
-      {/* Content */}
-      <div className="max-w-md mx-auto p-8 mt-8 bg-white rounded shadow-md">
-        <h1 className="text-3xl font-semibold mb-4">About Me</h1>
-        <p className="text-gray-600 mb-6">
-          Hi there! I'm <span className="text-blue-600">[Your Name]</span>, a passionate [Your Occupation] based in [Your Location]. I love [Your Interests/Hobbies] and enjoy [Your Activities].
-        </p>
-        <p className="text-gray-600">
-          Feel free to reach out to me at <a href="mailto:[Your Email]" className="text-blue-600 hover:underline">[Your Email]</a> for any inquiries or just to say hello!
-        </p>
+    <>
+      <Navbar></Navbar>
+      <div className="fixed bottom-0 left-0 flex items-center justify-center min-h-screen bg-gray-100">
+      <ThreeScene />
       </div>
-
-      {/* Chat Bar */}
-      {isChatOpen && (
-        <div className="fixed bottom-0 right-4 p-4 bg-white rounded-tl-lg shadow-md w-64">
-          {/* Your chat content goes here */}
-          <p className="text-gray-600">Chat with me!</p>
-        </div>
-      )}
-    </div>
+      <div className="h-[calc(100vh+500px)]"></div>
+    </>
   );
 };
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 left-0 flex items-center justify-center align-middle h-[calc(4vh)] w-screen bg-red-200 z-1">
+
+    </div>
+  );
+};
+
+export default Navbar;
